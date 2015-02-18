2f6197380f53da670e9abae4be5f43bd46f9d97f
3rafanan <3rafanan@gmail.com>
2015-02-18 13:21:40 -0800

    Partial tutorial 5b (step 16)
    Signed-off-by: 3rafanan <3rafanan@gmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,9 +11,16 @@ class Admin extends Application
         parent::__construct();
     }
 
+    function add()
+    {
+        $quote = $this->quotes->create();
+        $this->present($quote);
+    }
     function index()
     {
-        $this->data['pagebody'] = 'homepage';
+        $this->data['title'] = 'Quotations Maintenance';
+        $this->data['quotes'] = $this->quotes->all();
+        $this->data['pagebody'] = 'admin_list';
         $this->render();
     }
 }
